675b9cf54bd870b34880c5e9217241e128aafc59
krish <krishdu.p@gmail.com>
2022-09-23 21:34:23 +0530

    vercel-config

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const logger = require('morgan');
-const homeRouter = require('./router/homeRouter');
+const express = require("express");
+const logger = require("morgan");
+const homeRouter = require("./router/homeRouter");
 const app = express();
 
-const PORT  = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
-app.use(logger('tiny'));
+app.use(logger("tiny"));
 //set a view engine
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended : false}))
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/static', express.static('public'))
+app.use("/static", express.static("public"));
 
-app.use('/', homeRouter);
+app.use("/", homeRouter);
 
-app.get('/about', (req, res) => {
-    res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(PORT, (err) => {
-    if(err) console.error(err);
-    console.log(`listening on port http://localhost:${PORT}`);
+  if (err) console.error(err);
+  console.log(`listen on port http://localhost:${PORT}`);
 });
